337e05eb60bf8d6187990b6e1a08b82a0bcbf17a
Craig Melbourne <craig.melbourne@gmail.com>
2014-07-02 15:57:49 +0100

    minor revision

--- blaze/locations.js
+++ blaze/locations.js
@@ -1,4 +1,8 @@
-var destinations = ['New York', 'Las Vegas', 'Orlando',
-  'San Francisco', 'Chicago', 'Boston', 'Los Angeles', 'Miami', 'Seattle',
-  'Dallas'
+var destinations = [  
+       'Austin', 'Boston', 'Chicago', 'Detroit', 'Dallas', 'Houston', 
+'Las Vegas', 'Los Angeles', 'Miami', 'New York', 'Orlando', 'Portland',
+'San Antonio', 'San Diego', 'San Francisco', 'Seattle', 'Washington' 
+  
 ];
+
+
