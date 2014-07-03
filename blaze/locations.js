f0fb62f7255398f8e79e037cf7f29c109898a8d2
Craig Melbourne <craig.melbourne@gmail.com>
2014-07-03 17:10:05 +0100

    updated type ahead

--- blaze/locations.js
+++ blaze/locations.js
@@ -1,8 +1,37 @@
 var destinations = [  
-       'Austin', 'Boston', 'Chicago', 'Detroit', 'Dallas', 'Houston', 
-'Las Vegas', 'Los Angeles', 'Miami', 'New York', 'Orlando', 'Portland',
-'San Antonio', 'San Diego', 'San Francisco', 'Seattle', 'Washington' 
-  
+      'Atlantic City', 'Austin', 'Baltimore', 'Boston', 'Chicago', 'Dallas', 
+      'Denver', 'Honolulu', 'Las Vegas', 'Los Angeles', 'Miami', 'Minneapolis', 
+      'Montreal', 'New York', 'Nashville', 'New Orleans',  'Orlando', 'Philadelphia', 
+      'Portland', 'San Francisco', 'Seattle', 'San Antonio', 'Vancouver', 'Washington'
 ];
+
+var cities = [
+	{"city": "Atlantic City", "lat":-74.422928, "lon": 39.364285},
+    {"city": "Austin", "lat":-97.743057, "lon":30.267153},
+    {"city": "Baltimore", "lat":-76.612190, "lon":39.290386},
+    {"city": "Boston", "lat":-71.059776, "lon":42.358433},
+    {"city": "Chicago", "lat":-87.629799,"lon":41.878113},
+    {"city": "Dallas", "lat":-96.769920,"lon":32.802956},
+    {"city": "Denver", "lat":-104.984718, "lon": 39.737568},
+    {"city": "Honolulu", "lat":-157.858337,"lon":21.306944},
+    {"city": "Las Vegas", "lat":-115.172813,"lon":36.114647},
+    {"city": "Los Angeles", "lat":-118.243683,"lon":34.052235},
+    {"city": "Miami", "lat":-80.226440,"lon":25.788969},
+    {"city": "Minneapolis", "lat":-93.266670,"lon":44.983334},
+    {"city": "Montreal", "lat":-73.553993, "lon":45.508671},
+    {"city": "New York", "lat":-74.005974, "lon":40.714352},
+    {"city": "Nashville", "lat":-86.783333, "lon":36.166668},
+    {"city": "New Orleans", "lat":-80.226440, "lon":25.788969},
+    {"city": "Orlando", "lat":-81.379234, "lon":28.538336},
+    {"city": "Philadelphia", "lat":-75.163788, "lon":39.952335},
+    {"city": "Portland", "lat":-122.676208, "lon": 45.523453},
+    {"city": "San Francisco", "lat":-122.419418, "lon": 37.774929},
+    {"city": "Seattle", "lat":-122.332069, "lon":47.606209},
+    {"city": "San Antonio", "lat":-98.493629, "lon":29.424122},
+    {"city": "Vancouver", "lat":-123.113930, "lon":49.261227},
+    {"city": "Washington", "lat":-77.036369,"lon":38.895111}
+]
+
+
 
 
