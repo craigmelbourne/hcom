c30b530353535f17a2af32881d577409835efa28
Craig Melbourne <craig.melbourne@gmail.com>
2014-08-14 17:02:45 +0100

    map view updates

--- polymer/android/ean.js
+++ polymer/android/ean.js
@@ -1,7 +1,120 @@
 
 var geocoder = new google.maps.Geocoder();
+var map;
+var markers = [];
 
-function fetchHotels () {
+function geocodeString(callback) {
+	geocoder.geocode( { 'address': 'london, uk'}, function(pos, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+        	console.log("success");
+        	callback(pos);
+        } else {
+        	console.log("failed");
+        	callback(status);
+        }
+
+     });
+}
+
+function addMarker(lat, lng) {
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(lat, lng),
+    map: map
+  });
+  markers.push(marker);
+}
+
+function setAllMap(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+function clearMarkers() {
+  setAllMap(null);
+}
+
+function showMarkers() {
+  setAllMap(map);
+}
+
+function getHotels(){
+
+	fetchHotelsList(function(hotels){
+
+		$.each(hotels, function(i, hotel) {
+
+			addMarker(hotel.latitude, hotel.longitude);
+		
+		});
+
+	});
+
+}
+
+function fetchHotelsList(callback) {
+	var lon;
+    var lat;
+    var hotels;
+    //var geo = geocodeString();
+    geocodeString(function(pos){
+    	console.log(pos);
+    	lon = pos[0].geometry.location.A;
+        lat = pos[0].geometry.location.k;
+    	$.ajax({
+
+			crossDomain: true,
+        	url: "https://api.eancdn.com/ean-services/rs/hotel/v3/list",
+			data: {
+            	cid: 55505,
+            	minorRev: 99,
+            	apiKey: "xgdsee58vcvfhpr4hhvvhych",
+            	locale: "en_US",
+            	currencyCode: "GBP",
+            	arrivalDate: "09/30/2014",
+            	departureDate: "10/01/2014",
+            	longitude: lon, //result[0].lon,
+            	latitude: lat, //result[0].lat,
+            	searchRadius: 2,
+            	searchRadiusUnit: "MI",
+            	numberOfResults: 50
+        	},
+
+        	// whether this is a POST or GET request
+        	type: "GET",
+
+        	// the type of data we expect back
+        	dataType : "jsonp",
+        	timeout:3000,
+ 
+        	// code to run if the request succeeds;
+        	// the response is passed to the function
+        	success: function( json ) {
+        		console.log (json);
+
+        		var hotels = json.HotelListResponse.HotelList.HotelSummary;
+        		callback(hotels);
+        	
+        	}, 
+
+        	error: function( xhr, status, errorThrown ) {
+            	 //alert( "Sorry, there was a problem!" );
+            	console.log( "Error: " + errorThrown );
+            	console.log( "Status: " + status );
+            	console.dir( xhr );
+        	},
+ 
+        	// code to run regardless of success or failure
+        	complete: function( xhr, status ) {
+            
+        	}
+
+		});
+
+  	});
+}
+
+function fetchHotels() {
 	var lon;
     var lat;
 
@@ -169,10 +282,6 @@ function initializeMap() {
         if (status == google.maps.GeocoderStatus.OK) {
             //console.log (pos[0].geometry.location.k + " " + pos[0].geometry.location.B)
             map.setCenter(pos[0].geometry.location);
-            var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(pos[0].geometry.location.k, pos[0].geometry.location.A),
-                    map: map
-                });
         } 
     });
 
@@ -190,5 +299,12 @@ function buildHotelDetails(hotel){
 	$.each(rooms, function(i, room) {
       $("#rooms").append("<div class='details-block'><h2>" + room.description + "</h2></div>")
     });
+
+	map.setZoom(15);
+    addMarker(hotel.HotelSummary.latitude, hotel.HotelSummary.longitude);
+    map.setCenter({lat: hotel.HotelSummary.latitude, lng: hotel.HotelSummary.longitude});
+    
+    map.panBy(-300, 0);
+
 }
 
