beb4ea35bc2b5a1236c2fa46cd8661357bf8d91d
Craig Melbourne <craig.melbourne@gmail.com>
2014-09-18 21:50:27 +0100

    srp-pdp

--- mobile/hcom-services.js
+++ mobile/hcom-services.js
@@ -12,45 +12,17 @@ var fetchHotelList = function (destination, cb){
 
 }
 
+var fetchHotelDetails = function (id, cb){
+	var url = "http://localhost:8899/device/hotelDetails.html?hotelId=" + id + "&na=os%3DFacebook%7Ccv%3D1171%7Csv%3D2%7Cid%3DCFUUID9BC82AC4A123485586F5F6AFEF2084E0"
+	console.log("fetch hotel details");
 
-
-
-function fetchSearchResults(destination) {
-
-	var url = buildSearchUrl(destination);
-	var hotels;
-
-	$.getJSON(url).done(function(json){
-		hotels = json.result.hotels;
-		//console.log (json.result.hotels[0])
-		//$.each( hotels, function( i, hotel ) {
-  			//console.log( i + ": " + hotel.hotelName );
-
-			//$("#hotelList").append("<li id=" + hotel.hotelId + ">" + hotel.hotelName + "</li>")
-					//});
-
-		return hotels;
+	$.ajax({
+  		dataType: "json",
+  		url: url,
+  		success: cb
 	});
-
-	
-
-};
-
-function fetchHotelDetails(hotelId) {
-
-	var url = "http://localhost:8899/device/hotelDetails.html?hotelId=" + hotelId + "&na=os%3DFacebook%7Ccv%3D1171%7Csv%3D2%7Cid%3DCFUUID9BC82AC4A123485586F5F6AFEF2084E0"
-	var hotel;
-
-	$.getJSON(url).done(function(json){
-
-		hotel = json;
-		//console.log(json.result);
-		//$("#details h1").text(json.result.roomRates.hotelSummary.hotelName);
-	});
-
-	return hotel;
-
 }
+
 
 
 function buildSearchUrl(destination) {
