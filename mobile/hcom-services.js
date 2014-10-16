d5b9c0d514d5b5ee50be0fbc277fd2e8f9532869
Craig Melbourne <craig.melbourne@gmail.com>
2014-10-16 21:21:20 +0100

    android updates

--- mobile/hcom-services.js
+++ mobile/hcom-services.js
@@ -13,7 +13,8 @@ var fetchHotelList = function (destination, cb){
 }
 
 var fetchHotelDetails = function (id, cb){
-	var url = "http://localhost:8899/device/hotelDetails.html?hotelId=" + id + "&na=os%3DFacebook%7Ccv%3D1171%7Csv%3D2%7Cid%3DCFUUID9BC82AC4A123485586F5F6AFEF2084E0"
+	var url = buildHotelDetailsUrl(id);
+  //var url = "http://localhost:8899/device/hotelDetails.html?hotelId=" + id + "&na=os%3DFacebook%7Ccv%3D1171%7Csv%3D2%7Cid%3DCFUUID9BC82AC4A123485586F5F6AFEF2084E0"
 	console.log("fetch hotel details");
 
 	$.ajax({
@@ -37,5 +38,41 @@ function buildSearchUrl(destination) {
   	}
 
   	return url;
+}
+
+function buildHotelDetailsUrl(id) {
+    var url = '', 
+     //search_destination = (destination) ? destination : SearchForm.destination,
+     hostname = window.location.hostname;
+
+  if (hostname == 'localhost') {    
+      url = "http://localhost:8899/device/hotelDetails.html?hotelId=" + id + "&na=os%3DFacebook%7Ccv%3D1171%7Csv%3D2%7Cid%3DCFUUID9BC82AC4A123485586F5F6AFEF2084E0";
+    } else {
+      url = "/api/hotelDetails.html?hotelId=" + id + "&na=os%3DFacebook%7Ccv%3D1171%7Csv%3D2%7Cid%3DCFUUID9BC82AC4A123485586F5F6AFEF2084E0";
+    }
+
+    return url;
+}
+
+//var map;
+function initializeMap(destination) {
+
+	console.log("initilaise map")
+      
+    map = new google.maps.Map(document.getElementById('map_canvas'), {
+        //center: loc,
+        zoom: 16,
+        mapTypeId: 'roadmap',
+        mapTypeId: google.maps.MapTypeId.ROADMAP, 
+        panControl: false,
+        mapTypeControl: false,
+        streetViewControl:false,
+        zoomControl: false,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL,
+            position: google.maps.ControlPosition.RIGHT_BOTTOM
+        }
+    });
+
 }
 
